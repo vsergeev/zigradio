6033eb5ddf8065777929cb1cd422667e3961e2fc
Vanya A. Sergeev <v@sergeev.io>
2023-02-05 18:34:08 -0600

    blocks/signal: add FIRFilterBlock

--- src/blocks/signal/index.zig
+++ src/blocks/signal/index.zig
@@ -6,3 +6,4 @@ test {
 pub const DownsamplerBlock = @import("downsampler.zig").DownsamplerBlock;
 pub const FrequencyTranslatorBlock = @import("frequencytranslator.zig").FrequencyTranslatorBlock;
 pub const FrequencyDiscriminatorBlock = @import("frequencydiscriminator.zig").FrequencyDiscriminatorBlock;
+pub const FIRFilterBlock = @import("firfilter.zig").FIRFilterBlock;
