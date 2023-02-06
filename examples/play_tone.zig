29d569976e10e53d67a17055da9733e2256de1ff
Vanya A. Sergeev <v@sergeev.io>
2023-02-05 18:40:15 -0600

    examples: add play_tone example

---
+++ examples/play_tone.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+
+const radio = @import("radio");
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+
+    var source = radio.blocks.SignalSource.init(radio.blocks.SignalSource.WaveformFunction.Cosine, 440, 44100, .{});
+    var sink = radio.blocks.PulseAudioSink.init();
+
+    var top = radio.Flowgraph.init(gpa.allocator());
+    defer top.deinit();
+    try top.connect(&source.block, &sink.block);
+
+    try top.start();
+    radio.testing.waitForInterrupt();
+    try top.stop();
+}
