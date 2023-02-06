9529800a9a179cbc79e4085ed5b9e9a116af94e1
Vanya A. Sergeev <v@sergeev.io>
2023-02-05 18:33:28 -0600

    blocks/signal: add FrequencyTranslatorBlock

--- src/blocks/signal/index.zig
+++ src/blocks/signal/index.zig
@@ -4,3 +4,4 @@ test {
 }
 
 pub const DownsamplerBlock = @import("downsampler.zig").DownsamplerBlock;
+pub const FrequencyTranslatorBlock = @import("frequencytranslator.zig").FrequencyTranslatorBlock;
