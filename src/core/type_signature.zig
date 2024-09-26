b527b7bbfaffd622aab7dc87ab554e3424184372
Vanya A. Sergeev <v@sergeev.io>
2024-09-26 03:11:55 -0500

    core: simplify state in ComptimeTypeSignature and RuntimeTypeSignature

--- src/core/type_signature.zig
+++ src/core/type_signature.zig
@@ -4,41 +4,25 @@ const std = @import("std");
 // Comptime Type Signatures
 ////////////////////////////////////////////////////////////////////////////////
 
-pub const ComptimeInput = struct {
-    name: []const u8,
-    data_type: type,
-};
-
-pub const ComptimeOutput = struct {
-    name: []const u8,
-    data_type: type,
-};
-
 pub const ComptimeTypeSignature = struct {
-    inputs: []const ComptimeInput,
-    outputs: []const ComptimeOutput,
+    inputs: []type,
+    outputs: []type,
 
     pub fn init(comptime process_fn: anytype) ComptimeTypeSignature {
         const process_args = @typeInfo(@TypeOf(process_fn)).Fn.params[1..];
 
-        comptime var comptime_inputs: [process_args.len]ComptimeInput = undefined;
-        comptime var comptime_outputs: [process_args.len]ComptimeOutput = undefined;
+        comptime var comptime_inputs: [process_args.len]type = undefined;
+        comptime var comptime_outputs: [process_args.len]type = undefined;
         comptime var num_inputs: usize = 0;
         comptime var num_outputs: usize = 0;
 
         inline for (process_args) |arg| {
-            comptime var arg_is_input = @typeInfo(arg.type orelse unreachable).Pointer.is_const;
+            const arg_is_input = @typeInfo(arg.type orelse unreachable).Pointer.is_const;
             if (arg_is_input) {
-                comptime_inputs[num_inputs] = ComptimeInput{
-                    .name = comptime std.fmt.comptimePrint("in{d}", .{num_inputs + 1}),
-                    .data_type = @typeInfo(arg.type orelse unreachable).Pointer.child,
-                };
+                comptime_inputs[num_inputs] = @typeInfo(arg.type orelse unreachable).Pointer.child;
                 num_inputs += 1;
             } else {
-                comptime_outputs[num_outputs] = ComptimeOutput{
-                    .name = comptime std.fmt.comptimePrint("out{d}", .{num_outputs + 1}),
-                    .data_type = @typeInfo(arg.type orelse unreachable).Pointer.child,
-                };
+                comptime_outputs[num_outputs] = @typeInfo(arg.type orelse unreachable).Pointer.child;
                 num_outputs += 1;
             }
         }
@@ -53,7 +37,7 @@ pub const ComptimeTypeSignature = struct {
         comptime var data_types: [self.inputs.len]type = undefined;
 
         inline for (self.inputs, 0..) |input, i| {
-            data_types[i] = input.data_type;
+            data_types[i] = input;
         }
 
         return data_types[0..];
@@ -63,7 +47,7 @@ pub const ComptimeTypeSignature = struct {
         comptime var data_types: [self.outputs.len]type = undefined;
 
         inline for (self.outputs, 0..) |output, i| {
-            data_types[i] = output.data_type;
+            data_types[i] = output;
         }
 
         return data_types[0..];
@@ -107,37 +91,16 @@ pub const RuntimeDataType = enum {
     }
 };
 
-pub const RuntimeInput = struct {
-    name: []const u8,
-    data_type: RuntimeDataType,
-};
-
-pub const RuntimeOutput = struct {
-    name: []const u8,
-    data_type: RuntimeDataType,
-};
-
 pub const RuntimeTypeSignature = struct {
-    inputs: []const RuntimeInput,
-    outputs: []const RuntimeOutput,
+    inputs: []RuntimeDataType,
+    outputs: []RuntimeDataType,
 
     pub fn init(comptime type_signature: ComptimeTypeSignature) RuntimeTypeSignature {
-        comptime var runtime_inputs: [type_signature.inputs.len]RuntimeInput = undefined;
-        comptime var runtime_outputs: [type_signature.outputs.len]RuntimeOutput = undefined;
+        comptime var runtime_inputs: [type_signature.inputs.len]RuntimeDataType = undefined;
+        comptime var runtime_outputs: [type_signature.outputs.len]RuntimeDataType = undefined;
 
-        inline for (type_signature.inputs, 0..) |input, i| {
-            runtime_inputs[i] = RuntimeInput{
-                .name = input.name,
-                .data_type = comptime RuntimeDataType.map(input.data_type),
-            };
-        }
-
-        inline for (type_signature.outputs, 0..) |output, i| {
-            runtime_outputs[i] = RuntimeOutput{
-                .name = output.name,
-                .data_type = comptime RuntimeDataType.map(output.data_type),
-            };
-        }
+        inline for (type_signature.inputs, 0..) |input, i| runtime_inputs[i] = comptime RuntimeDataType.map(input);
+        inline for (type_signature.outputs, 0..) |output, i| runtime_outputs[i] = comptime RuntimeDataType.map(output);
 
         return RuntimeTypeSignature{
             .inputs = runtime_inputs[0..],
@@ -155,7 +118,7 @@ test "ComptimeTypeSignature.init" {
     const TestProcess00 = struct {
         fn process(_: *@This()) void {}
     };
-    comptime var ts00 = ComptimeTypeSignature.init(TestProcess00.process);
+    const ts00 = ComptimeTypeSignature.init(TestProcess00.process);
     try std.testing.expectEqual(0, ts00.inputs.len);
     try std.testing.expectEqual(0, ts00.outputs.len);
 
@@ -163,49 +126,41 @@ test "ComptimeTypeSignature.init" {
     const TestProcess10 = struct {
         fn process(_: *@This(), _: []const u32) void {}
     };
-    comptime var ts10 = ComptimeTypeSignature.init(TestProcess10.process);
+    const ts10 = ComptimeTypeSignature.init(TestProcess10.process);
     try std.testing.expectEqual(1, ts10.inputs.len);
-    try std.testing.expectEqualSlices(u8, "in1", ts10.inputs[0].name);
-    try std.testing.expectEqual(u32, ts10.inputs[0].data_type);
+    try std.testing.expectEqual(u32, ts10.inputs[0]);
     try std.testing.expectEqual(0, ts10.outputs.len);
 
     // 0 inputs, 1 output
     const TestProcess01 = struct {
         fn process(_: *@This(), _: []u32) void {}
     };
-    comptime var ts01 = ComptimeTypeSignature.init(TestProcess01.process);
+    const ts01 = ComptimeTypeSignature.init(TestProcess01.process);
     try std.testing.expectEqual(0, ts01.inputs.len);
     try std.testing.expectEqual(1, ts01.outputs.len);
-    try std.testing.expectEqualSlices(u8, "out1", ts01.outputs[0].name);
-    try std.testing.expectEqual(u32, ts01.outputs[0].data_type);
+    try std.testing.expectEqual(u32, ts01.outputs[0]);
 
     // 1 input, 1 output
     const TestProcess11 = struct {
         fn process(_: *@This(), _: []const u16, _: []u8) void {}
     };
-    comptime var ts11 = ComptimeTypeSignature.init(TestProcess11.process);
+    const ts11 = ComptimeTypeSignature.init(TestProcess11.process);
     try std.testing.expectEqual(1, ts11.inputs.len);
-    try std.testing.expectEqualSlices(u8, "in1", ts11.inputs[0].name);
-    try std.testing.expectEqual(u16, ts11.inputs[0].data_type);
+    try std.testing.expectEqual(u16, ts11.inputs[0]);
     try std.testing.expectEqual(1, ts11.outputs.len);
-    try std.testing.expectEqualSlices(u8, "out1", ts11.outputs[0].name);
-    try std.testing.expectEqual(u8, ts11.outputs[0].data_type);
+    try std.testing.expectEqual(u8, ts11.outputs[0]);
 
     // 2 inputs, 2 outputs
     const TestProcess22 = struct {
         fn process(_: *@This(), _: []const u16, _: []const u32, _: []u8, _: []bool) void {}
     };
-    comptime var ts22 = ComptimeTypeSignature.init(TestProcess22.process);
+    const ts22 = ComptimeTypeSignature.init(TestProcess22.process);
     try std.testing.expectEqual(2, ts22.inputs.len);
-    try std.testing.expectEqualSlices(u8, "in1", ts22.inputs[0].name);
-    try std.testing.expectEqual(u16, ts22.inputs[0].data_type);
-    try std.testing.expectEqualSlices(u8, "in2", ts22.inputs[1].name);
-    try std.testing.expectEqual(u32, ts22.inputs[1].data_type);
+    try std.testing.expectEqual(u16, ts22.inputs[0]);
+    try std.testing.expectEqual(u32, ts22.inputs[1]);
     try std.testing.expectEqual(2, ts22.outputs.len);
-    try std.testing.expectEqualSlices(u8, "out1", ts22.outputs[0].name);
-    try std.testing.expectEqual(u8, ts22.outputs[0].data_type);
-    try std.testing.expectEqualSlices(u8, "out2", ts22.outputs[1].name);
-    try std.testing.expectEqual(bool, ts22.outputs[1].data_type);
+    try std.testing.expectEqual(u8, ts22.outputs[0]);
+    try std.testing.expectEqual(bool, ts22.outputs[1]);
 }
 
 test "ComptimeTypeSignature.getInputTypes" {
@@ -255,44 +210,36 @@ test "RuntimeDataType.map" {
 // RuntimeTypeSignature Tests
 ////////////////////////////////////////////////////////////////////////////////
 
-fn expectEqualPorts(comptime T: type, expected: []const T, actual: []const T) anyerror!void {
-    try std.testing.expectEqual(expected.len, actual.len);
-    for (expected, 0..) |exp, i| {
-        try std.testing.expectEqualSlices(u8, exp.name, actual[i].name);
-        try std.testing.expectEqual(exp.data_type, actual[i].data_type);
-    }
-}
-
 test "RuntimeTypeSignature.init" {
     // 1 input, 0 outputs
     const TestProcess10 = struct {
         fn process(_: *@This(), _: []const u32) void {}
     };
     const ts10 = RuntimeTypeSignature.init(ComptimeTypeSignature.init(TestProcess10.process));
-    try expectEqualPorts(RuntimeInput, &[1]RuntimeInput{RuntimeInput{ .name = "in1", .data_type = RuntimeDataType.Unsigned32 }}, ts10.inputs);
-    try expectEqualPorts(RuntimeOutput, &[0]RuntimeOutput{}, ts10.outputs);
+    try std.testing.expectEqualSlices(RuntimeDataType, &[1]RuntimeDataType{RuntimeDataType.Unsigned32}, ts10.inputs);
+    try std.testing.expectEqualSlices(RuntimeDataType, &[0]RuntimeDataType{}, ts10.outputs);
 
     // 0 inputs, 1 outputs
     const TestProcess01 = struct {
         fn process(_: *@This(), _: []u32) void {}
     };
     const ts01 = RuntimeTypeSignature.init(ComptimeTypeSignature.init(TestProcess01.process));
-    try expectEqualPorts(RuntimeInput, &[0]RuntimeInput{}, ts01.inputs);
-    try expectEqualPorts(RuntimeOutput, &[1]RuntimeOutput{RuntimeOutput{ .name = "out1", .data_type = RuntimeDataType.Unsigned32 }}, ts01.outputs);
+    try std.testing.expectEqualSlices(RuntimeDataType, &[0]RuntimeDataType{}, ts01.inputs);
+    try std.testing.expectEqualSlices(RuntimeDataType, &[1]RuntimeDataType{RuntimeDataType.Unsigned32}, ts01.outputs);
 
     // 1 inputs, 1 outputs
     const TestProcess11 = struct {
         fn process(_: *@This(), _: []const f32, _: []u32) void {}
     };
     const ts11 = RuntimeTypeSignature.init(ComptimeTypeSignature.init(TestProcess11.process));
-    try expectEqualPorts(RuntimeInput, &[1]RuntimeInput{RuntimeInput{ .name = "in1", .data_type = RuntimeDataType.Float32 }}, ts11.inputs);
-    try expectEqualPorts(RuntimeOutput, &[1]RuntimeOutput{RuntimeOutput{ .name = "out1", .data_type = RuntimeDataType.Unsigned32 }}, ts11.outputs);
+    try std.testing.expectEqualSlices(RuntimeDataType, &[1]RuntimeDataType{RuntimeDataType.Float32}, ts11.inputs);
+    try std.testing.expectEqualSlices(RuntimeDataType, &[1]RuntimeDataType{RuntimeDataType.Unsigned32}, ts11.outputs);
 
     // 2 inputs, 2 outputs
     const TestProcess22 = struct {
         fn process(_: *@This(), _: []const f32, _: []const u16, _: []u32, _: []f64) void {}
     };
     const ts22 = RuntimeTypeSignature.init(ComptimeTypeSignature.init(TestProcess22.process));
-    try expectEqualPorts(RuntimeInput, &[2]RuntimeInput{ RuntimeInput{ .name = "in1", .data_type = RuntimeDataType.Float32 }, RuntimeInput{ .name = "in2", .data_type = RuntimeDataType.Unsigned16 } }, ts22.inputs);
-    try expectEqualPorts(RuntimeOutput, &[2]RuntimeOutput{ RuntimeOutput{ .name = "out1", .data_type = RuntimeDataType.Unsigned32 }, RuntimeOutput{ .name = "out2", .data_type = RuntimeDataType.Float64 } }, ts22.outputs);
+    try std.testing.expectEqualSlices(RuntimeDataType, &[2]RuntimeDataType{ RuntimeDataType.Float32, RuntimeDataType.Unsigned16 }, ts22.inputs);
+    try std.testing.expectEqualSlices(RuntimeDataType, &[2]RuntimeDataType{ RuntimeDataType.Unsigned32, RuntimeDataType.Float64 }, ts22.outputs);
 }
