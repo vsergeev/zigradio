3e487be3808727278cb323bc281e7c3f72663e7f
Vanya A. Sergeev <v@sergeev.io>
2025-01-04 21:36:40 -0600

    build: remove shared library linking

--- build.zig
+++ build.zig
@@ -3,48 +3,19 @@ const std = @import("std");
 const Example = struct {
     name: []const u8,
     path: []const u8,
-    libs: []const []const u8,
 };
 
 const examples = [_]Example{
-    .{ .name = "example-rtlsdr_wbfm_mono", .path = "examples/rtlsdr_wbfm_mono.zig", .libs = &.{ "pulse-simple", "pulse", "rtlsdr" } },
-    .{ .name = "example-play_tone", .path = "examples/play_tone.zig", .libs = &.{ "pulse-simple", "pulse" } },
+    .{ .name = "example-rtlsdr_wbfm_mono", .path = "examples/rtlsdr_wbfm_mono.zig" },
+    .{ .name = "example-play_tone", .path = "examples/play_tone.zig" },
 };
-
-// Adapted from private std.Build.execPkgConfigList()
-// FIXME not exactly portable or stable
-fn getPlatformPackages(self: *std.Build) ![]const []const u8 {
-    var code: u8 = undefined;
-    const stdout = try self.runAllowFail(&[_][]const u8{ "pkg-config", "--list-all" }, &code, .Ignore);
-    var list = std.ArrayList([]const u8).init(self.allocator);
-    errdefer list.deinit();
-    var line_it = std.mem.tokenize(u8, stdout, "\r\n");
-    while (line_it.next()) |line| {
-        if (std.mem.trim(u8, line, " \t").len == 0) continue;
-        var tok_it = std.mem.tokenize(u8, line, " \t");
-        try list.append(tok_it.next() orelse return error.PkgConfigInvalidOutput);
-    }
-    return list.toOwnedSlice();
-}
 
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    // Create platform options with packages list
-    const platform_options = b.addOptions();
-    platform_options.addOption([]const []const u8, "packages", try getPlatformPackages(b));
-
-    // Create platform options module
-    const platform_options_module = platform_options.createModule();
-
     // Create radio module
-    const radio_module = b.addModule("radio", .{
-        .root_source_file = b.path("src/radio.zig"),
-        .imports = &.{
-            .{ .name = "platform_options", .module = platform_options_module },
-        },
-    });
+    const radio_module = b.addModule("radio", .{ .root_source_file = b.path("src/radio.zig") });
     radio_module.addImport("radio", radio_module);
 
     // Build examples
@@ -58,7 +29,6 @@ pub fn build(b: *std.Build) !void {
         });
         exe.root_module.addImport("radio", radio_module);
         exe.linkLibC();
-        for (example.libs) |libname| exe.linkSystemLibrary(libname);
 
         examples_step.dependOn(&b.addInstallArtifact(exe, .{}).step);
     }
--- src/core/platform.zig
+++ src/core/platform.zig
@@ -1,17 +1,8 @@
 const std = @import("std");
-const platform_options = @import("platform_options");
 
 ////////////////////////////////////////////////////////////////////////////////
 // Platform Functions
 ////////////////////////////////////////////////////////////////////////////////
-
-pub fn hasPackage(comptime name: []const u8) bool {
-    @setEvalBranchQuota(1_000_000);
-    for (platform_options.packages) |pkg| {
-        if (std.mem.eql(u8, pkg, name)) return true;
-    }
-    return false;
-}
 
 pub fn waitForInterrupt() void {
     var mask = std.posix.empty_sigset;
