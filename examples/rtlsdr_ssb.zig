057271eb5fd19e382c406d095779e096a931dd77
Vanya A. Sergeev <v@sergeev.io>
2025-01-26 00:54:33 -0600

    examples: use AGCBlock in rtlsdr_ssb

--- examples/rtlsdr_ssb.zig
+++ examples/rtlsdr_ssb.zig
@@ -23,6 +23,7 @@ pub fn main() !void {
     var sb_filter = radio.blocks.ComplexBandpassFilterBlock(129).init(if (sideband == .LSB) .{ 0, -bandwidth } else .{ 0, bandwidth }, .{});
     var am_demod = radio.blocks.ComplexToRealBlock.init();
     var af_filter = radio.blocks.LowpassFilterBlock(f32, 128).init(bandwidth, .{});
+    var af_gain = radio.blocks.AGCBlock(f32).init(.Fast, .{});
     var sink = radio.blocks.PulseAudioSink(1).init();
 
     var top = radio.Flowgraph.init(gpa.allocator(), .{ .debug = true });
@@ -31,7 +32,8 @@ pub fn main() !void {
     try top.connect(&tuner.composite, &sb_filter.block);
     try top.connect(&sb_filter.block, &am_demod.block);
     try top.connect(&am_demod.block, &af_filter.block);
-    try top.connect(&af_filter.block, &sink.block);
+    try top.connect(&af_filter.block, &af_gain.block);
+    try top.connect(&af_gain.block, &sink.block);
 
     try top.start();
     radio.platform.waitForInterrupt();
