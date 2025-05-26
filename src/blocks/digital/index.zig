94295a8eabdc4828d2a69a61253521e6a81b9b6d
Vanya A. Sergeev <v@sergeev.io>
2025-05-25 23:22:33 -0500

    blocks/digital: add SlicerBlock and BinarySlicer

--- src/blocks/digital/index.zig
+++ src/blocks/digital/index.zig
@@ -4,3 +4,5 @@ test {
 }
 
 pub const DifferentialDecoderBlock = @import("differentialdecoder.zig").DifferentialDecoderBlock;
+pub const SlicerBlock = @import("slicer.zig").SlicerBlock;
+pub const BinarySlicer = @import("slicer.zig").BinarySlicer;
---
+++ src/blocks/digital/slicer.zig
@@ -0,0 +1,59 @@
+const std = @import("std");
+
+const Block = @import("../../radio.zig").Block;
+const ProcessResult = @import("../../radio.zig").ProcessResult;
+
+////////////////////////////////////////////////////////////////////////////////
+// Slicer Block
+////////////////////////////////////////////////////////////////////////////////
+
+pub const BinarySlicer = struct {
+    pub inline fn process(value: f32) u1 {
+        return @intFromBool(value > 0.0);
+    }
+};
+
+pub fn SlicerBlock(comptime slicer: type) type {
+    return struct {
+        const Self = @This();
+
+        block: Block,
+
+        pub fn init() Self {
+            return .{ .block = Block.init(@This()) };
+        }
+
+        fn SlicerInputType() type {
+            return @typeInfo(@TypeOf(slicer.process)).@"fn".params[0].type.?;
+        }
+
+        fn SlicerOutputType() type {
+            return @typeInfo(@TypeOf(slicer.process)).@"fn".return_type.?;
+        }
+
+        pub fn process(_: *Self, x: []const SlicerInputType(), z: []SlicerOutputType()) !ProcessResult {
+            for (x, 0..) |e, i| {
+                z[i] = slicer.process(e);
+            }
+
+            return ProcessResult.init(&[1]usize{x.len}, &[1]usize{x.len});
+        }
+    };
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Tests
+////////////////////////////////////////////////////////////////////////////////
+
+const BlockTester = @import("../../radio.zig").testing.BlockTester;
+
+const vectors = @import("../../vectors/blocks/digital/slicer.zig");
+
+test "SlicerBlock" {
+    // Binary Slicer
+    {
+        var block = SlicerBlock(BinarySlicer).init();
+        var tester = try BlockTester(&[1]type{f32}, &[1]type{u1}).init(&block.block, 1e-6);
+        try tester.check(2, .{&vectors.input_float32}, .{&vectors.output_bit}, .{});
+    }
+}
