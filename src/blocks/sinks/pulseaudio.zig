edb5f81fb53907faec65a42b3e1835ba52d16a23
Vanya A. Sergeev <v@sergeev.io>
2025-06-22 02:53:15 -0500

    blocks/sinks/pulseaudio: refactor interleaved samples state

--- src/blocks/sinks/pulseaudio.zig
+++ src/blocks/sinks/pulseaudio.zig
@@ -66,7 +66,7 @@ pub fn PulseAudioSink(comptime N: comptime_int) type {
 
         block: Block,
         pa_conn: ?*pa_simple = null,
-        interleaved: ?std.ArrayList(f32) = null,
+        interleaved: std.ArrayList(f32) = undefined,
 
         pub fn init() Self {
             return .{
@@ -106,7 +106,7 @@ pub fn PulseAudioSink(comptime N: comptime_int) type {
 
         pub fn deinitialize(self: *Self, _: std.mem.Allocator) void {
             // Free interleaved array
-            if (N > 1) self.interleaved.?.deinit();
+            if (N > 1) self.interleaved.deinit();
 
             // Close and free our PulseAudio connection
             if (self.pa_conn) |pa_conn| pa_simple_free(pa_conn);
@@ -126,15 +126,15 @@ pub fn PulseAudioSink(comptime N: comptime_int) type {
 
         pub fn _process_stereo(self: *Self, x: []const f32, y: []const f32) !ProcessResult {
             // Interleave samples
-            try self.interleaved.?.resize(x.len * 2);
+            try self.interleaved.resize(x.len * 2);
             for (x, y, 0..) |_, _, i| {
-                self.interleaved.?.items[2 * i] = x[i];
-                self.interleaved.?.items[2 * i + 1] = y[i];
+                self.interleaved.items[2 * i] = x[i];
+                self.interleaved.items[2 * i + 1] = y[i];
             }
 
             // Write to our PulseAudio connection
             var error_code: c_int = undefined;
-            const ret = pa_simple_write(self.pa_conn.?, self.interleaved.?.items.ptr, self.interleave.?.items.len * @sizeOf(f32), &error_code);
+            const ret = pa_simple_write(self.pa_conn.?, self.interleaved.items.ptr, self.interleaved.items.len * @sizeOf(f32), &error_code);
             if (ret < 0) {
                 std.debug.print("pa_simple_write(): {s}\n", .{pa_strerror(error_code)});
                 return PulseAudioError.WriteError;
